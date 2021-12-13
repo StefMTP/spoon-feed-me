dc16f790d3616a9a77583d253c442619e9b280fa
Stefanos Michalas <sm@think-plus.gr>
2021-12-13 14:51:13 +0200

    f it

--- src/stores/recipestore.ts
+++ src/stores/recipestore.ts
@@ -3,7 +3,7 @@ import { writable } from 'svelte/store';
 export const recipes = writable([]);
 
 export const fetchRecipes = async (number: number) => {
-	const apiKey = process.env.SPOONACULAR_API_KEY;
+	const apiKey = '62a9bf270d174ed2b4aba95e63da33c6';
 	const url = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=${number}`;
 	const res = await fetch(url, {
 		method: 'GET',
